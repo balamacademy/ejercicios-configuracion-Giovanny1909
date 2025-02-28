4caaafc43bf96bd644e6e6089106e0a272dbec1e
Giovanny1909 <mendez.giovanny2001@gmail.com>
2025-02-27 22:03:13 -0600

    Add files via upload

---
+++ holamundo.cpp
@@ -0,0 +1,14 @@
+//Esta es la biblioteca de entrada y
+//salida de datos
+#include <iostream>
+
+/*
+Todo lo
+que este aqui
+es un comentario
+*/
+
+int main (int argc,char **argv){
+         std::cout <<"Hola Mundo!";
+         return 0;
+}
---
+++ imprimirvalores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main (){
+         //string es cadena de caracteres
+         std::string nombre="Juan";
+
+         //int es enteros abrv. Integer
+         int edad = 34;
+
+         std::cout << "El nombre es " << nombre << " de edad " << edad;
+         return 0;
+}
